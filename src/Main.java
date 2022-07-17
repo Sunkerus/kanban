1eb087c392b669c8257444556d7aff5371576e8b
unknown <sunkerus@gmail.com>
2022-07-17 21:13:44 +0300

    qwe

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Поехали!");
+        System.out.println("Создаём один эпик с подзадачей");
+        Epic newEpic1 = new Epic("name", "description", new Subtask ("подзадача", "description" ));
 
     }
 
