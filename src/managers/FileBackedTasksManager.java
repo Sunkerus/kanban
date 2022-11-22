b89fc65b74589072540f243ff88ebb396847f51c
Sunkerus <sunkerus@gmail.com>
2022-11-22 13:46:29 +0400

    add: new call method

--- src/managers/FileBackedTasksManager.java
+++ src/managers/FileBackedTasksManager.java
@@ -265,15 +265,15 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         fileTaskManager.createSubtask(subtask3_1);
         fileTaskManager.getSubtaskById(3);
 
-        fileTaskManager.getTaskById(2).setStatus(StatusTask.IN_PROGRESS);
-        System.out.println(fileTaskManager.getTaskById(2));
-
         System.out.println(fileTaskManager.getAllEpic());
         System.out.println(fileTaskManager.getAllSubtask());
         System.out.println(fileTaskManager.getAllTask());
         System.out.println("====================История====================");
         System.out.println(fileTaskManager.getHistory());
 
+        fileTaskManager.getTaskById(2).setStatus(StatusTask.IN_PROGRESS);
+        System.out.println("\nИзменим статус задачи");
+        System.out.println(fileTaskManager.getTaskById(2));
 
         File loadFilePath = new File(".\\Resources\\saveConfig.csv");
 
