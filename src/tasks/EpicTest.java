12940a0195850d3717cfcb79e9bde5b55dc7f9fc
Sunkerus <sunkerus@gmail.com>
2022-12-29 22:53:30 +0400

    adding some tests

---
+++ src/tasks/EpicTest.java
@@ -0,0 +1,16 @@
+package tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpicTest {
+
+    @Test
+    public void someTest() {
+
+
+    }
+
+
+}
